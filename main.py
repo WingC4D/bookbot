b688d6609a91b61ef839833a9b9512c25849c8ab
Wing.C4D <mikmuk1@gmail.com>
2025-03-11 11:38:28 +0200

    now itarating over every letter

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-from stats import main
+from stats import letter_count 
+import inspect
+print(inspect.getsource(letter_count))  
+def main(filepath):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    try:
+        with open(filepath) as f:
+            booktext = f.read()
+            letters_dict = letter_count(booktext)
+            words_lst = booktext.split()
+            print(f"found {len(words_lst)} total words")   
+            print("--------- Character Count -------")
+            print(letters_dict)
+    except FileNotFoundError:
+        print(f"Error: The file '{filepath}' was not found. Double-check the file path and ensure it exists")
+    return
 if __name__ == "__main__":
     main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -1,23 +1,8 @@
-def main(filepath):
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}")
-    print("----------- Word Count ----------")
-    try:
-        with open(filepath) as f:
-            booktext = f.read()
-            words_lst = booktext.split()
-            print(f"found {len(words_lst)} total words")
-            letters_list = list(booktext.lower())
-            letters_dict = {}
-            for letter in letters_list:
-                if letter in letters_dict:
-                    letters_dict[letter] += 1
-                else:
-                    letters_dict[letter] = 1
-        print("--------- Character Count -------")
-        print(letters_dict)
-    except FileNotFoundError:
-        print(f"Error: The file '{filepath}' was not found. Double-check the file path and ensure it exists")
-    return
-if __name__ == "__main__":
-    main("books/frankenstein.txt")
+def letter_count(text):
+    letters_dict = {}
+    for letter in text.lower():
+        if letter in letters_dict:
+            letters_dict[letter] += 1
+        else:
+            letters_dict[letter] = 1
+    return letters_dict
