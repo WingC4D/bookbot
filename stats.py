5420bf231587bf5c998e953e958c85e6edf83dc8
Wing.C4D <mikmuk1@gmail.com>
2025-03-11 11:04:29 +0200

    main update

--- stats.py
+++ stats.py
@@ -2,29 +2,20 @@ def main(filepath):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}")
     print("----------- Word Count ----------")
-    t_counter = 0
-    p_counter = 0
-    c_counter =0
     try:
         with open(filepath) as f:
             booktext = f.read()
             words_lst = booktext.split()
             print(f"found {len(words_lst)} total words")
             letters_list = list(booktext.lower())
+            letters_dict = {}
             for letter in letters_list:
-                if letter == "t":
-                    t_counter += 1
-                if letter == "e":
-                    p_counter += 1
-                if letter == "c":
-                    c_counter += 1
-        dict_list = [
-        {"t" : t_counter},
-        {"p" : p_counter},
-        {"c" : c_counter}
-        ]
+                if letter in letters_dict:
+                    letters_dict[letter] += 1
+                else:
+                    letters_dict[letter] = 1
         print("--------- Character Count -------")
-        print(dict_list)
+        print(letters_dict)
     except FileNotFoundError:
         print(f"Error: The file '{filepath}' was not found. Double-check the file path and ensure it exists")
     return
