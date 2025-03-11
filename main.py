9fba849f7a634b0b2a7b9cc526ab233eb6e2eac0
Wing.C4D <mikmuk1@gmail.com>
2025-03-11 16:50:04 +0200

    finished bookbot

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import sys
 from stats import letter_count, sort_characters
-def main(filepath):
+def main():
+    print("script started")
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}")
     print("----------- Word Count ----------")
     try:
-        with open(filepath) as f:
+        with open(filepath,"r",encoding="utf8") as f:
             booktext = f.read()
             letters_dict = letter_count(booktext)
             words_lst = booktext.split()
@@ -12,11 +18,10 @@ def main(filepath):
             print("--------- Character Count -------")
             char_list = sort_characters(letters_dict)
             for char_info in char_list:
-                char = char_info["char"]
-                count = char_info["count"]
-                print(f"{char}: {count}")
+                print(f"{char_info["char"]}: {char_info["count"]}")
     except FileNotFoundError:
         print(f"Error: The file '{filepath}' was not found. Double-check the file path and ensure it exists")
+        sys.exit(1)
     return
 if __name__ == "__main__":
-    main("books/frankenstein.txt")
+    main()
